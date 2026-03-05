01df3dc5dffa9f68026b726bff36d1b844dd0677
WhangEli <1069760166@qq.com>
2026-03-05 16:43:25 +0800

    added some code in Array

--- Array/Array.h
+++ Array/Array.h
@@ -1,8 +1,24 @@
 ﻿#pragma once
 class Array
 {
+public:
+	Array(int cap) :mCapacity(cap), mSize(0)
+	{
+		mpArr = new int[cap]();
+	}
+
+	~Array()
+	{
+		delete[] mpArr;
+		mpArr = nullptr;
+	}
+
+public:
+	
 
 private:
 	int* mpArr;
+	int mCapacity;
+	int mSize;
 };
 
