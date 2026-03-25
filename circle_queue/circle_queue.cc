14d2853a4909845acf7dd59804afffc2f1b4b18b
WhangEli <1069760166@qq.com>
2026-03-25 09:43:12 +0800

    completed class CircleQueue

--- circle_queue/circle_queue.cc
+++ circle_queue/circle_queue.cc
@@ -1,2 +1,39 @@
 ﻿#include "circle_queue.h"
 
+CircleQueue::CircleQueue(int capacity = 10)
+	: capacity_(capacity)
+	, front_(0)
+	, rear_(0)
+{
+	arr_ = new int[capacity];
+}
+
+CircleQueue::~CircleQueue()
+{
+	delete[]arr_;
+	arr_ = nullptr;
+}
+
+void CircleQueue::Expand(int capacity)
+{
+	int* p = new int[capacity];
+	memcpy(p, arr_, sizeof(int) * capacity_);
+	delete[]arr_;
+	arr_ = p;
+}
+
+void CircleQueue::Enqueue(int val)
+{
+	arr_[rear_] = val;
+	rear_ = ++rear_ % capacity_;
+
+	if (rear_ == front_)
+	{
+		Expand(2 * capacity_);
+	}
+}
+
+void CircleQueue::Dequeue()
+{
+	front_ = ++front_ % capacity_;
+}
--- circle_queue/circle_queue.h
+++ circle_queue/circle_queue.h
@@ -3,6 +3,21 @@
 
 class CircleQueue
 {
+public:
+	CircleQueue(int capacity);
+	~CircleQueue();
 
+public:
+	void Enqueue(int val);
+	void Dequeue();
+
+private:
+	void Expand(int capacity);
+
+private:
+	int* arr_;
+	int front_;
+	int rear_;
+	int capacity_;
 };
 
