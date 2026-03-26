ecb5c38853c64b109dd034faad765f096992a062
WhangEli <1069760166@qq.com>
2026-03-26 22:21:15 +0800

    completed InsertSort

--- sort/sort_algorithm.cc
+++ sort/sort_algorithm.cc
@@ -2,10 +2,10 @@
 
 void BubbleSort(int arr[], int size)
 {
-	for (int i = 0; i < capacity - 1; i++)
+	for (int i = 0; i < size - 1; i++)
 	{
 		bool flag = false;
-		for (int j = 0; j < capacity - 1 - i; j++)
+		for (int j = 0; j < size - 1 - i; j++)
 		{
 			if (arr[j] > arr[j + 1])
 			{
@@ -35,15 +35,48 @@ void SelectionSort(int arr[], int size)
 				min = j;
 			}
 		}
-		int temp = arr[min];
-		arr[min] = arr[i];
-		arr[i] = temp;
+		if (min != i)
+		{
+			int temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+		}
 	}
 }
 
 void InsertionSort(int arr[], int size)
 {
+	for (int i = 1; i < size; i++)
+	{
+		int j = i - 1;
+		while (j>=0)
+		{
+			if (arr[i] >= arr[j])
+			{
+				int temp = arr[i];
+				for (int v = i; v >= j + 1; v--)
+				{
+					arr[v] = arr[v - 1];
+				}
+				arr[j + 1] = temp;
+				break;
+			}
+			else
+			{
+				j--;
+			}
+		}
 
+		if (j == -1)
+		{
+			int temp = arr[i];
+			for (int v = i; v > 0; v--)
+			{
+				arr[v] = arr[v - 1];
+			}
+			arr[0] = temp;
+		}
+	}
 }
 
 void ShellSort(int arr[], int size)
